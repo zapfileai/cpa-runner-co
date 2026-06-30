1b95e19d97f0dded52753520fc472362898396e5
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:19:03 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
-﻿const coloradoCities = [
-  // paste first 80 cities from cities.csv here
+const coloradoCities = [
+  "Agate", "Aguilar", "Alamosa", "Anton", "Antonito",
+  "Arapahoe", "Arriba", "Arvada", "Aspen", "Aurora",
+  "Avon", "Bailey", "Basalt", "Bayfield", "Bellvue",
+  "Berthoud", "Beulah", "Black Hawk", "Boulder", "Breckenridge",
+  "Briggsdale", "Brighton", "Broomfield", "Buena Vista", "Buffalo Creek",
+  "Byers", "Calhan", "Canon City", "Carbondale", "Cascade",
+  "Castle Rock", "Cedaredge", "Center", "Central City", "Cimarron",
+  "Clifton", "Coalmont", "Collbran", "Colorado Springs", "Commerce City",
+  "Como", "Conifer", "Cope", "Cortez", "Cory",
+  "Cowdrey", "Craig", "Crested Butte", "Cripple Creek", "Delta",
+  "Denver", "Dillon", "Divide", "Dolores", "Dumont",
+  "Durango", "Eagle", "Eaton", "Edwards", "Elbert",
+  "Elizabeth", "Empire", "Englewood", "Erie", "Estes Park",
+  "Evans", "Evergreen", "Fleming", "Florissant", "Fort Collins",
+  "Fort Lupton", "Fort Morgan", "Fowler", "Franktown", "Fraser",
+  "Frederick", "Frisco", "Fruita", "Galeton", "Gardner",
 ];
 
 const BATCH_SIZE = 5;
