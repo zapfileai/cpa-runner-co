c1f9bb222df3898640172af26f58dbd446419039
Sreehari Annam <sannam@wescodist.com>
2026-07-02 09:55:31 -0400

    batch 3: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,9 @@
 const coloradoCities = [
-  "Gilcrest", "Gill", "Glade Park", "Glen Haven", "Glenwood Springs",
-  "Golden", "Granby", "Grand Junction", "Grand Lake", "Greeley",
-  "Green Mountain Falls", "Gunnison", "Gypsum", "Henderson", "Hesperus",
-  "Holyoke", "Hygiene", "Idaho Springs", "Idledale", "Ignacio",
-  "Indian Hills", "Jamestown", "Johnstown", "Kersey", "Kittredge",
-  "La Jara", "Lafayette", "Lamar", "Laporte", "Larkspur",
-  "Leadville", "Littleton", "Loma", "Lone Tree", "Longmont",
-  "Louisville", "Loveland", "Lyons", "Mack", "Mancos",
-  "Manitou Springs", "Masonville", "Mccoy", "Mead", "Milliken",
-  "Minturn", "Monte Vista", "Montrose", "Monument", "Morrison",
-  "Nathrop", "New Castle", "Niwot", "Ouray", "Pagosa Springs",
-  "Palisade", "Palmer Lake", "Paonia", "Parachute", "Paradox",
-  "Parker", "Peyton", "Pine", "Platteville", "Pritchett",
-  "Pueblo", "Ramah", "Red Feather Lakes", "Ridgway", "Rifle",
-  "Rush", "Rye", "Salida", "Sedalia", "Severance",
-  "Sheridan Lake", "Silver Plume", "Silverthorne", "Snowmass Village", "Steamboat Springs",
+  "Sterling", "Stoneham", "Strasburg", "Telluride", "Thornton",
+  "Timnath", "Trinchera", "Trinidad", "Twin Lakes", "Vail",
+  "Wellington", "Westcliffe", "Westminster", "Wetmore", "Wheat Ridge",
+  "Wiggins", "Wiley", "Windsor", "Winter Park", "Woodland Park",
+  "Wray", "Yoder",
 ];
 
 const BATCH_SIZE = 5;
